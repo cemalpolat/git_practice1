d70627c090ddd247040a0938665993e5d34b2064
Cemal Polat <cemalpolat0001@gmail.com>
2022-04-08 16:06:12 +0100

    work 2 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 
     // Doing some work here (Type anything) yan taraf yesil oluyor.
     // bu , son committen sonra yaptigin yenilikler demek.
+    // Doing awesome work 2!
 
 }
