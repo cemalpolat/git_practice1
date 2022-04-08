c1770358eccab20307878382daee306b3b49ad35
Cemal Polat <cemalpolat0001@gmail.com>
2022-04-08 18:05:51 +0100

    work 3 is done and about to Github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld {
     // bu , son committen sonra yaptigin yenilikler demek.
     // Doing awesome work 2!
 
+    // Doing awesome work 2!
+    //Awesome work 3 is done and ready to be committed
+    // and ready to be pushed to remote repository
+
 }
